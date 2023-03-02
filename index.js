b0f7e0262d71f6b902dce10feec90cf8dedb3e32
AaKuGu <prathamtogupta11@gmail.com>
2023-03-02 21:57:28 +0530

    little bit change

--- index.js
+++ index.js
@@ -1,67 +1,64 @@
 const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// const bodyParser = require("body-parser");
-// const { dbConnection } = require("./db/dbConnection");
-// const userRouter = require("./routes/userRoutes");
-// const messageRouter = require("./routes/messageRoutes");
-// const socket = require("socket.io");
-// // const { check } = require("./config/config");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const { dbConnection } = require("./db/dbConnection");
+const userRouter = require("./routes/userRoutes");
+const messageRouter = require("./routes/messageRoutes");
+const socket = require("socket.io");
+// const { check } = require("./config/config");
 
 dotenv.config();
 
 const app = express();
 
-// // app.use(cors());
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT;
 
-// // dbConnection(process.env.MONGO_URL);
+dbConnection(process.env.MONGO_URL);
 
 app.get("/", (req, res) => {
   res.status(200).send({ msg: "server is working fine", status: true });
 });
 
-// // // app.use("/api/auth", userRouter);
-// // // app.use("/api/message", messageRouter);
+app.use("/api/auth", userRouter);
+app.use("/api/message", messageRouter);
 
-// // // const server = app.listen(PORT, () => {
-// // //   console.log(`server running on http://localhost:${PORT}`);
-// // // });
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
 });
 
-// // const io = socket(server, {
-// //   cors: {
-// //     origin: "http://localhost:3000",
-// //     credentials: true,
-// //   },
-// // });
 
-// // global.onlineUsers = new Map();
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
 
-// // io.on("connection", (socket) => {
-// //   global.chatSocket = socket;
-// //   socket.on("add-user", (userId) => {
-// //     // console.log("add-user : ", userId);
-// //     onlineUsers.set(userId, socket.id);
-// //   });
-// //   // console.log("reached to socket");
-// //   socket.on("send-msg", (data) => {
-// //     const sendUserSocket = onlineUsers.get(data.to);
-// //     // console.log("reached to socket if", data);
+global.onlineUsers = new Map();
 
-// //     if (sendUserSocket) {
-// //       // console.log("msg-receive socket if : ", data.message);
-// //       socket.to(sendUserSocket).emit("msg-receive", data.message);
-// //     }
-// //   });
-// // });
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    // console.log("add-user : ", userId);
+    onlineUsers.set(userId, socket.id);
+  });
+  // console.log("reached to socket");
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    console.log("reached to socket if", data);
+
+    if (sendUserSocket) {
+      // console.log("msg-receive socket if : ", data.message);
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
+    }
+  });
+});
 
 
 // working code 
